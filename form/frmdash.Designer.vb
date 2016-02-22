c84d5e1329a59bd878b9bce8b060412dc2887e42
jeffgavina <jeffgavina06@gmail.com>
2016-02-22 10:10:31 +0800

    test Commit

---
+++ form/frmdash.Designer.vb
@@ -0,0 +1,403 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmdash
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbllogo = New System.Windows.Forms.Label()
+        Me.btnadmin = New System.Windows.Forms.Button()
+        Me.btnpim = New System.Windows.Forms.Button()
+        Me.btnleave = New System.Windows.Forms.Button()
+        Me.btntime = New System.Windows.Forms.Button()
+        Me.btnrecruitment = New System.Windows.Forms.Button()
+        Me.btnperformance = New System.Windows.Forms.Button()
+        Me.btndashboard = New System.Windows.Forms.Button()
+        Me.grpbox1 = New System.Windows.Forms.GroupBox()
+        Me.btnconfig = New System.Windows.Forms.Button()
+        Me.btnnation = New System.Windows.Forms.Button()
+        Me.btnquali = New System.Windows.Forms.Button()
+        Me.btnuserman = New System.Windows.Forms.Button()
+        Me.btnorgan = New System.Windows.Forms.Button()
+        Me.btnjob = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnreset = New System.Windows.Forms.Button()
+        Me.btnsearch = New System.Windows.Forms.Button()
+        Me.cmbstatus = New System.Windows.Forms.ComboBox()
+        Me.cmbuserrole = New System.Windows.Forms.ComboBox()
+        Me.txtempname = New System.Windows.Forms.TextBox()
+        Me.txtusername = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btndelete = New System.Windows.Forms.Button()
+        Me.btnadd = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.grpbox1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lbllogo
+        '
+        Me.lbllogo.AutoSize = True
+        Me.lbllogo.Location = New System.Drawing.Point(77, 0)
+        Me.lbllogo.Name = "lbllogo"
+        Me.lbllogo.Size = New System.Drawing.Size(27, 13)
+        Me.lbllogo.TabIndex = 0
+        Me.lbllogo.Text = "logo"
+        '
+        'btnadmin
+        '
+        Me.btnadmin.Location = New System.Drawing.Point(0, 44)
+        Me.btnadmin.Name = "btnadmin"
+        Me.btnadmin.Size = New System.Drawing.Size(134, 35)
+        Me.btnadmin.TabIndex = 1
+        Me.btnadmin.Text = "Admin"
+        Me.btnadmin.UseVisualStyleBackColor = True
+        '
+        'btnpim
+        '
+        Me.btnpim.Location = New System.Drawing.Point(131, 44)
+        Me.btnpim.Name = "btnpim"
+        Me.btnpim.Size = New System.Drawing.Size(134, 35)
+        Me.btnpim.TabIndex = 2
+        Me.btnpim.Text = "PIM"
+        Me.btnpim.UseVisualStyleBackColor = True
+        '
+        'btnleave
+        '
+        Me.btnleave.Location = New System.Drawing.Point(259, 44)
+        Me.btnleave.Name = "btnleave"
+        Me.btnleave.Size = New System.Drawing.Size(134, 35)
+        Me.btnleave.TabIndex = 3
+        Me.btnleave.Text = "Leave"
+        Me.btnleave.UseVisualStyleBackColor = True
+        '
+        'btntime
+        '
+        Me.btntime.Location = New System.Drawing.Point(389, 44)
+        Me.btntime.Name = "btntime"
+        Me.btntime.Size = New System.Drawing.Size(134, 35)
+        Me.btntime.TabIndex = 4
+        Me.btntime.Text = "Time"
+        Me.btntime.UseVisualStyleBackColor = True
+        '
+        'btnrecruitment
+        '
+        Me.btnrecruitment.Location = New System.Drawing.Point(519, 44)
+        Me.btnrecruitment.Name = "btnrecruitment"
+        Me.btnrecruitment.Size = New System.Drawing.Size(134, 35)
+        Me.btnrecruitment.TabIndex = 5
+        Me.btnrecruitment.Text = "Recruitment"
+        Me.btnrecruitment.UseVisualStyleBackColor = True
+        '
+        'btnperformance
+        '
+        Me.btnperformance.Location = New System.Drawing.Point(649, 44)
+        Me.btnperformance.Name = "btnperformance"
+        Me.btnperformance.Size = New System.Drawing.Size(134, 35)
+        Me.btnperformance.TabIndex = 6
+        Me.btnperformance.Text = "Performance"
+        Me.btnperformance.UseVisualStyleBackColor = True
+        '
+        'btndashboard
+        '
+        Me.btndashboard.Location = New System.Drawing.Point(780, 44)
+        Me.btndashboard.Name = "btndashboard"
+        Me.btndashboard.Size = New System.Drawing.Size(134, 35)
+        Me.btndashboard.TabIndex = 7
+        Me.btndashboard.Text = "Dashboard"
+        Me.btndashboard.UseVisualStyleBackColor = True
+        '
+        'grpbox1
+        '
+        Me.grpbox1.BackColor = System.Drawing.Color.Khaki
+        Me.grpbox1.Controls.Add(Me.btnconfig)
+        Me.grpbox1.Controls.Add(Me.btnnation)
+        Me.grpbox1.Controls.Add(Me.btnquali)
+        Me.grpbox1.Controls.Add(Me.btnuserman)
+        Me.grpbox1.Controls.Add(Me.btnorgan)
+        Me.grpbox1.Controls.Add(Me.btnjob)
+        Me.grpbox1.Location = New System.Drawing.Point(2, 59)
+        Me.grpbox1.Name = "grpbox1"
+        Me.grpbox1.Size = New System.Drawing.Size(912, 54)
+        Me.grpbox1.TabIndex = 8
+        Me.grpbox1.TabStop = False
+        Me.grpbox1.Visible = False
+        '
+        'btnconfig
+        '
+        Me.btnconfig.Location = New System.Drawing.Point(647, 19)
+        Me.btnconfig.Name = "btnconfig"
+        Me.btnconfig.Size = New System.Drawing.Size(134, 35)
+        Me.btnconfig.TabIndex = 7
+        Me.btnconfig.Text = "Configuratio"
+        Me.btnconfig.UseVisualStyleBackColor = True
+        '
+        'btnnation
+        '
+        Me.btnnation.Location = New System.Drawing.Point(517, 19)
+        Me.btnnation.Name = "btnnation"
+        Me.btnnation.Size = New System.Drawing.Size(134, 35)
+        Me.btnnation.TabIndex = 6
+        Me.btnnation.Text = "Nationality"
+        Me.btnnation.UseVisualStyleBackColor = True
+        '
+        'btnquali
+        '
+        Me.btnquali.Location = New System.Drawing.Point(387, 19)
+        Me.btnquali.Name = "btnquali"
+        Me.btnquali.Size = New System.Drawing.Size(134, 35)
+        Me.btnquali.TabIndex = 5
+        Me.btnquali.Text = "Qualification"
+        Me.btnquali.UseVisualStyleBackColor = True
+        '
+        'btnuserman
+        '
+        Me.btnuserman.Location = New System.Drawing.Point(-2, 19)
+        Me.btnuserman.Name = "btnuserman"
+        Me.btnuserman.Size = New System.Drawing.Size(134, 35)
+        Me.btnuserman.TabIndex = 2
+        Me.btnuserman.Text = "User Management"
+        Me.btnuserman.UseVisualStyleBackColor = True
+        '
+        'btnorgan
+        '
+        Me.btnorgan.Location = New System.Drawing.Point(257, 19)
+        Me.btnorgan.Name = "btnorgan"
+        Me.btnorgan.Size = New System.Drawing.Size(134, 35)
+        Me.btnorgan.TabIndex = 4
+        Me.btnorgan.Text = "Organization"
+        Me.btnorgan.UseVisualStyleBackColor = True
+        '
+        'btnjob
+        '
+        Me.btnjob.Location = New System.Drawing.Point(129, 19)
+        Me.btnjob.Name = "btnjob"
+        Me.btnjob.Size = New System.Drawing.Size(134, 35)
+        Me.btnjob.TabIndex = 3
+        Me.btnjob.Text = "Job"
+        Me.btnjob.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnreset)
+        Me.GroupBox1.Controls.Add(Me.btnsearch)
+        Me.GroupBox1.Controls.Add(Me.cmbstatus)
+        Me.GroupBox1.Controls.Add(Me.cmbuserrole)
+        Me.GroupBox1.Controls.Add(Me.txtempname)
+        Me.GroupBox1.Controls.Add(Me.txtusername)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 119)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(1050, 217)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "System Users"
+        '
+        'btnreset
+        '
+        Me.btnreset.Location = New System.Drawing.Point(257, 138)
+        Me.btnreset.Name = "btnreset"
+        Me.btnreset.Size = New System.Drawing.Size(134, 35)
+        Me.btnreset.TabIndex = 9
+        Me.btnreset.Text = "Reset"
+        Me.btnreset.UseVisualStyleBackColor = True
+        '
+        'btnsearch
+        '
+        Me.btnsearch.Location = New System.Drawing.Point(69, 138)
+        Me.btnsearch.Name = "btnsearch"
+        Me.btnsearch.Size = New System.Drawing.Size(134, 35)
+        Me.btnsearch.TabIndex = 8
+        Me.btnsearch.Text = "Search"
+        Me.btnsearch.UseVisualStyleBackColor = True
+        '
+        'cmbstatus
+        '
+        Me.cmbstatus.FormattingEnabled = True
+        Me.cmbstatus.Items.AddRange(New Object() {"All", "Enable", "Disable"})
+        Me.cmbstatus.Location = New System.Drawing.Point(840, 70)
+        Me.cmbstatus.Name = "cmbstatus"
+        Me.cmbstatus.Size = New System.Drawing.Size(194, 21)
+        Me.cmbstatus.TabIndex = 7
+        '
+        'cmbuserrole
+        '
+        Me.cmbuserrole.FormattingEnabled = True
+        Me.cmbuserrole.Items.AddRange(New Object() {"All", "Admin", "ESS"})
+        Me.cmbuserrole.Location = New System.Drawing.Point(327, 70)
+        Me.cmbuserrole.Name = "cmbuserrole"
+        Me.cmbuserrole.Size = New System.Drawing.Size(194, 21)
+        Me.cmbuserrole.TabIndex = 6
+        '
+        'txtempname
+        '
+        Me.txtempname.Location = New System.Drawing.Point(587, 70)
+        Me.txtempname.Name = "txtempname"
+        Me.txtempname.Size = New System.Drawing.Size(194, 20)
+        Me.txtempname.TabIndex = 5
+        '
+        'txtusername
+        '
+        Me.txtusername.Location = New System.Drawing.Point(69, 70)
+        Me.txtusername.Name = "txtusername"
+        Me.txtusername.Size = New System.Drawing.Size(194, 20)
+        Me.txtusername.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(837, 45)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Status"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(584, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(84, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Employee Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(324, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "User Role"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(66, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "User Name"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btndelete)
+        Me.GroupBox2.Controls.Add(Me.btnadd)
+        Me.GroupBox2.Location = New System.Drawing.Point(0, 359)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(1052, 358)
+        Me.GroupBox2.TabIndex = 10
+        Me.GroupBox2.TabStop = False
+        '
+        'btndelete
+        '
+        Me.btndelete.Location = New System.Drawing.Point(257, 19)
+        Me.btndelete.Name = "btndelete"
+        Me.btndelete.Size = New System.Drawing.Size(134, 35)
+        Me.btndelete.TabIndex = 10
+        Me.btndelete.Text = "Delete"
+        Me.btndelete.UseVisualStyleBackColor = True
+        '
+        'btnadd
+        '
+        Me.btnadd.Location = New System.Drawing.Point(69, 19)
+        Me.btnadd.Name = "btnadd"
+        Me.btnadd.Size = New System.Drawing.Size(134, 35)
+        Me.btnadd.TabIndex = 9
+        Me.btnadd.Text = "Add"
+        Me.btnadd.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 440)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1054, 290)
+        Me.DataGridView1.TabIndex = 11
+        '
+        'frmdash
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1350, 729)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btndashboard)
+        Me.Controls.Add(Me.btnperformance)
+        Me.Controls.Add(Me.btnrecruitment)
+        Me.Controls.Add(Me.btntime)
+        Me.Controls.Add(Me.btnleave)
+        Me.Controls.Add(Me.btnpim)
+        Me.Controls.Add(Me.btnadmin)
+        Me.Controls.Add(Me.lbllogo)
+        Me.Controls.Add(Me.grpbox1)
+        Me.Name = "frmdash"
+        Me.Text = "frmdash"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.grpbox1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbllogo As System.Windows.Forms.Label
+    Friend WithEvents btnadmin As System.Windows.Forms.Button
+    Friend WithEvents btnpim As System.Windows.Forms.Button
+    Friend WithEvents btnleave As System.Windows.Forms.Button
+    Friend WithEvents btntime As System.Windows.Forms.Button
+    Friend WithEvents btnrecruitment As System.Windows.Forms.Button
+    Friend WithEvents btnperformance As System.Windows.Forms.Button
+    Friend WithEvents btndashboard As System.Windows.Forms.Button
+    Friend WithEvents grpbox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnconfig As System.Windows.Forms.Button
+    Friend WithEvents btnnation As System.Windows.Forms.Button
+    Friend WithEvents btnquali As System.Windows.Forms.Button
+    Friend WithEvents btnorgan As System.Windows.Forms.Button
+    Friend WithEvents btnjob As System.Windows.Forms.Button
+    Friend WithEvents btnuserman As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnreset As System.Windows.Forms.Button
+    Friend WithEvents btnsearch As System.Windows.Forms.Button
+    Friend WithEvents cmbstatus As System.Windows.Forms.ComboBox
+    Friend WithEvents cmbuserrole As System.Windows.Forms.ComboBox
+    Friend WithEvents txtempname As System.Windows.Forms.TextBox
+    Friend WithEvents txtusername As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents btndelete As System.Windows.Forms.Button
+    Friend WithEvents btnadd As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+End Class
