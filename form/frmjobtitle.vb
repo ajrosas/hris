490b57b230fae1c7a136c8079e6e62eda0b8e141
ajrosas <rosas.andrewjay@gmail.com>
2016-02-26 10:10:03 +0800

    Update master branch

--- form/frmjobtitle.vb
+++ form/frmjobtitle.vb
@@ -1,9 +1,10 @@
 ﻿Public Class frmjobtitle
 
-<<<<<<< HEAD
-=======
     Private Sub btnconfig_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnconfig.Click
 
     End Sub
->>>>>>> 27330fe6ccf49d9011537e27aa5923e1e99cdb2f
+
+    Private Sub btndelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btndelete.Click
+
+    End Sub
 End Class
